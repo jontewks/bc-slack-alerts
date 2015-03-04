b144b869f166af548ec176fe1d1ce7cea768c25d
Jon Tewksbury <jontewks@gmail.com>
2015-03-03 21:33:15 -0800

    Fix procfile.

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.post('/', function(req, res) {
 });
 
 var server = app.listen(app.get('port'), function () {
+  var host = server.address().address;
   var port = server.address().port;
-  console.log('Example app listening at http://:%s', port);
+
+  console.log('Listening at http://%s:%s', host, port);
 });
