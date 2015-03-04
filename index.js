23e149ccaab220fa92b7226191ad0dad9f10a1f0
Jon Tewksbury <jontewks@gmail.com>
2015-03-03 21:17:47 -0800

    Base of loggly - slack integration.

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000))
+
+app.post('/', function(req, res) {
+  console.log(req);
+});
+
+var server = app.listen(app.get('port'), function () {
+  var port = server.address().port;
+  console.log('Example app listening at http://localhost:%s', port);
+});
