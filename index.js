2c6025a101e870eb3cd46a4dd5af766d2e283027
Jon Tewksbury <jontewks@gmail.com>
2015-03-03 22:17:54 -0800

    Log whole request from loggly.

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ var bodyParser = require('body-parser');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
 app.get('/', function(req, res) {
   res.send('You got it.');
 });
 
 app.post('/', function(req, res) {
-  console.log(req.body);
+  console.log(req);
 });
 
 var server = app.listen(app.get('port'));
