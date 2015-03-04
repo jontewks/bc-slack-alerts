34ef918ce6073b57778a889b15431677db0f777b
Jon Tewksbury <jontewks@gmail.com>
2015-03-03 22:51:47 -0800

    Forward requests correctly to slack.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+var request = require('request');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,7 +9,48 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
-  console.log(req);
+  var payload = {
+    channel: "#hackers",
+    username: "Doom Guy",
+    text: "Test Post",
+    icon_emoji: ":godmode:"
+  };
+  
+  var type = req.query.type;
+
+  switch(type) {
+    case 'boxerror':
+      payload.text = 'Box Error';
+      break;
+    case 'elevatedclientalerts':
+      payload.text = 'Elevated Client Alerts';
+      break;
+    case 'elevatedserveralerts':
+      payload.text = 'Elevated Server Alerts';
+      break;
+    case 'jobfailed':
+      payload.text = 'Job Failed';
+      break;
+    case 'memoryquotaexceeded':
+      payload.text = 'Memory Quota Exceeded';
+      break;
+    case 'requesttimeouts':
+      payload.text = 'Request Timeouts';
+      break;
+    case 'slowrequests':
+      payload.text = 'Slow Requests';
+      break;
+    default:
+      break;
+  }
+
+  request({
+    url: process.env.WEBSITE,
+    method: 'POST',
+    body: JSON.stringify(payload)
+  }, function() {
+    res.sendStatus(201);
+  });
 });
 
 var server = app.listen(app.get('port'));
