814998df6a499b489434871adb65191d291b1906
Jon Tewksbury <jontewks@gmail.com>
2015-03-03 22:08:55 -0800

    Add bodyparser.

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.send('You got it.');
 });
 
 app.post('/', function(req, res) {
-  console.log(req);
+  console.log(req.body);
 });
 
 var server = app.listen(app.get('port'));
