356565aff37add4cba432f889eeb0cda10a78636
Jon Tewksbury <jontewks@gmail.com>
2015-03-03 21:37:19 -0800

    Add get for nothing.

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 
+app.get('/', function(req, res) {
+  res.send('You got it.');
+});
+
 app.post('/', function(req, res) {
-  console.log(req);
+  res.send(req);
 });
 
 var server = app.listen(app.get('port'), function () {
