c9ee6d6f17b57e598bf082d681a7188e8ee1590d
Jon Tewksbury <jontewks@gmail.com>
2015-03-06 14:23:22 -0800

    Add heroku route.

--- server/routes/heroku.js
+++ server/routes/heroku.js
@@ -7,8 +7,46 @@ router.get('/', function(req, res) {
     url: 'https://status.heroku.com/api/v3/current-status',
     method: 'GET'
   }, function(error, response, body) {
-    console.log(body); // {"status":{"Production":"green","Development":"green"},"issues":[]}
-    res.sendStatus(200).end();
+    var result = JSON.parse(body);
+    var color;
+
+    if (result.status.Production === 'green' && result.status.Development === 'green') {
+      color = 'good';
+    } else {
+      color = 'danger';
+    }
+
+    var payload = {
+      channel: '#hackers',
+      username: 'Heroku',
+      icon_emoji: ':heroku:',
+      attachments: [{
+        fallback: 'Heroku Status',
+        color: color,
+        title: 'Heroku Status',
+        title_link: 'https://status.heroku.com/',
+        fields: [{
+          title: 'Production',
+          value: result.status.Production === 'green' ? 'Operational' : 'Experiencing issues',
+          short: 'true'
+        }, {
+          title: 'Development',
+          value: result.status.Development === 'green' ? 'Operational' : 'Experiencing issues',
+          short: 'true'
+        }, {
+          title: 'Issues',
+          value: result.issues.length ? issues : 'No issues'
+        }]
+      }]
+    };
+    
+    request({
+      url: process.env.URL,
+      method: 'POST',
+      body: JSON.stringify(payload)
+    }, function() {
+      res.sendStatus(200).end();
+    });
   });
 });
 
