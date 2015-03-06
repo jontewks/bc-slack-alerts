c9424b2167b6289c9942125f5dc09134140d62a9
Jon Tewksbury <jontewks@gmail.com>
2015-03-06 14:36:59 -0800

    Add secret check and remove response code.

--- server/routes/heroku.js
+++ server/routes/heroku.js
@@ -3,51 +3,53 @@ var router = express.Router();
 var request = require('request');
 
 router.get('/', function(req, res) {
-  request({
-    url: 'https://status.heroku.com/api/v3/current-status',
-    method: 'GET'
-  }, function(error, response, body) {
-    var result = JSON.parse(body);
-    var color;
-
-    if (result.status.Production === 'green' && result.status.Development === 'green') {
-      color = 'good';
-    } else {
-      color = 'danger';
-    }
-
-    var payload = {
-      channel: '#hackers',
-      username: 'Heroku',
-      icon_emoji: ':heroku:',
-      attachments: [{
-        fallback: 'Heroku Status',
-        color: color,
-        title: 'Heroku Status',
-        title_link: 'https://status.heroku.com/',
-        fields: [{
-          title: 'Production',
-          value: result.status.Production === 'green' ? 'Operational' : 'Experiencing issues',
-          short: 'true'
-        }, {
-          title: 'Development',
-          value: result.status.Development === 'green' ? 'Operational' : 'Experiencing issues',
-          short: 'true'
-        }, {
-          title: 'Issues',
-          value: result.issues.length ? issues : 'No issues'
-        }]
-      }]
-    };
-    
+  if (req.query.secret !== process.env.SECRET) {
+    res.sendStatus(404).end();
+  } else {
     request({
-      url: process.env.URL,
-      method: 'POST',
-      body: JSON.stringify(payload)
-    }, function() {
-      res.sendStatus(200).end();
+      url: 'https://status.heroku.com/api/v3/current-status',
+      method: 'GET'
+    }, function(error, response, body) {
+      var result = JSON.parse(body);
+      var color;
+
+      if (result.status.Production === 'green' && result.status.Development === 'green') {
+        color = 'good';
+      } else {
+        color = 'danger';
+      }
+
+      var payload = {
+        channel: '#hackers',
+        username: 'Heroku',
+        icon_emoji: ':heroku:',
+        attachments: [{
+          fallback: 'Heroku Status',
+          color: color,
+          title: 'Heroku Status',
+          title_link: 'https://status.heroku.com/',
+          fields: [{
+            title: 'Production',
+            value: result.status.Production === 'green' ? 'Operational' : 'Experiencing issues'
+          }, {
+            title: 'Development',
+            value: result.status.Development === 'green' ? 'Operational' : 'Experiencing issues'
+          }, {
+            title: 'Issues',
+            value: result.issues.length ? issues : 'No issues'
+          }]
+        }]
+      };
+      
+      request({
+        url: process.env.URL,
+        method: 'POST',
+        body: JSON.stringify(payload)
+      }, function() {
+        res.end();
+      });
     });
-  });
+  }
 });
 
 module.exports = router;
