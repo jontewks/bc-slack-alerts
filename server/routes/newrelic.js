686d16cf7e029ccf0cc31fc29a802b2b3a2fa707
Jon Tewksbury <jontewks@gmail.com>
2015-03-10 13:40:26 -0700

    Add base for newrelic alerts.

---
+++ server/routes/newrelic.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+router.post('/', function(req, res) {
+  console.log(req.body);
+  // if (req.query.secret !== process.env.SECRET) {
+  //   res.sendStatus(404).end();
+  // } else if (req.body.build.status !== 'testing' && req.body.build.status !== 'success') {
+  //   var payload = {
+  //     channel: '#hackers',
+  //     username: 'Codeship',
+  //     icon_emoji: ':codeship:',
+  //     attachments: [{
+  //       fallback: 'Alert',
+  //       color: 'danger',
+  //       title: 'Build Failed',
+  //       title_link: req.body.build.build_url,
+  //       fields: [{
+  //         title: 'Branch',
+  //         value: req.body.build.branch,
+  //         short: 'true'
+  //       }, {
+  //         title: 'Shame upon',
+  //         value: '<' + req.body.build.commit_url + '|' + req.body.build.committer + '>',
+  //         short: 'true'
+  //       }]
+  //     }]
+  //   };
+    
+  //   request({
+  //     url: process.env.URL,
+  //     method: 'POST',
+  //     body: JSON.stringify(payload)
+  //   }, function() {
+  //     res.sendStatus(201).end();
+  //   });
+  // } else {
+  //   res.sendStatus(201).end();
+  // }
+});
+
+module.exports = router;
