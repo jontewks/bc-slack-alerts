10ff579f93579a63f73132e415b7235fca167e8d
Jon Tewksbury <jontewks@gmail.com>
2015-03-12 16:03:16 -0700

    Move to DMs.

--- server/routes/codeship.js
+++ server/routes/codeship.js
@@ -2,12 +2,20 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
+var nameMap = {
+  alexmingoia: '@alex',
+  bradfordh: '@bradfordh',
+  jessepedler: '@jessepedler',
+  joebalancio: '@joe',
+  jontewks: '@jon'
+};
+
 router.post('/', function(req, res) {
   if (req.query.secret !== process.env.SECRET) {
     res.sendStatus(404).end();
   } else if (req.body.build.status !== 'testing' && req.body.build.status !== 'success') {
     var payload = {
-      channel: '#hackers',
+      channel: nameMap[req.body.build.committer],
       username: 'Codeship',
       icon_emoji: ':codeship:',
       attachments: [{
@@ -20,8 +28,8 @@ router.post('/', function(req, res) {
           value: req.body.build.branch,
           short: 'true'
         }, {
-          title: 'Shame upon',
-          value: '<' + req.body.build.commit_url + '|' + req.body.build.committer + '>',
+          title: 'This is the breaking commit',
+          value: '<' + req.body.build.commit_url + '|Commit>',
           short: 'true'
         }]
       }]
