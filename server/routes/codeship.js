0cb6a019eaf14015a2233605b48b957ee7abf1e6
Jon Tewksbury <jontewks@gmail.com>
2015-03-13 14:52:38 -0700

    Add back hackers alert for dev or master build fail.

--- server/routes/codeship.js
+++ server/routes/codeship.js
@@ -14,26 +14,51 @@ router.post('/', function(req, res) {
   if (req.query.secret !== process.env.SECRET) {
     res.sendStatus(404).end();
   } else if (req.body.build.status !== 'testing' && req.body.build.status !== 'success') {
-    var payload = {
-      channel: nameMap[req.body.build.committer],
-      username: 'Codeship',
-      icon_emoji: ':codeship:',
-      attachments: [{
-        fallback: 'Alert',
-        color: 'danger',
-        title: 'Build Failed',
-        title_link: req.body.build.build_url,
-        fields: [{
-          title: 'Branch',
-          value: req.body.build.branch,
-          short: 'true'
-        }, {
-          title: 'This is the breaking commit',
-          value: '<' + req.body.build.commit_url + '|Commit>',
-          short: 'true'
+    var payload;
+
+    if (req.body.build.branch === 'master' || req.body.build.branch === 'dev') {
+      payload = {
+        channel: '#hackers',
+        username: 'Codeship',
+        icon_emoji: ':codeship:',
+        attachments: [{
+          fallback: 'Alert',
+          color: 'danger',
+          title: 'Build Failed',
+          title_link: req.body.build.build_url,
+          fields: [{
+            title: 'Branch',
+            value: req.body.build.branch,
+            short: 'true'
+          }, {
+            title: 'Shame upon',
+            value: '<' + req.body.build.commit_url + '|' + req.body.build.committer + '>',
+            short: 'true'
+          }]
         }]
-      }]
-    };
+      };
+    } else {
+      payload = {
+        channel: nameMap[req.body.build.committer],
+        username: 'Codeship',
+        icon_emoji: ':codeship:',
+        attachments: [{
+          fallback: 'Alert',
+          color: 'danger',
+          title: 'Build Failed',
+          title_link: req.body.build.build_url,
+          fields: [{
+            title: 'Branch',
+            value: req.body.build.branch,
+            short: 'true'
+          }, {
+            title: 'This is the breaking commit',
+            value: '<' + req.body.build.commit_url + '|Commit>',
+            short: 'true'
+          }]
+        }]
+      };
+    }
     
     request({
       url: process.env.URL,
